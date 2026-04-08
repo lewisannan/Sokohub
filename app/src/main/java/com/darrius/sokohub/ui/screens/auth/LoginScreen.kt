a049d195f8a3fb3c5d90cbbf9bb5942a7686c324
kipchirchirdarius884 <kipchirchirdarius74@gmail.com>
2026-04-08 11:11:54 +0300

    initial commit

---
+++ app/src/main/java/com/darrius/sokohub/ui/screens/auth/LoginScreen.kt
@@ -0,0 +1,119 @@
+package com.darrius.sokohub.ui.screens.auth
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.darrius.sokohub.R
+import com.darrius.sokohub.ui.theme.neworange
+
+@Composable
+fun ServiceScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+
+
+    ) {
+        Image(
+            painter = painterResource(R.drawable.products),
+            contentDescription = "",
+            modifier = Modifier.size(150.dp),
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+
+
+        Spacer(modifier = Modifier.height(5.dp))
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            modifier = Modifier.width(350.dp),
+            leadingIcon = {Icon(imageVector = Icons.Default.Email, contentDescription = "")},
+            label = {Text(text = "Email Address")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = neworange,
+                focusedBorderColor = Color.DarkGray,
+                unfocusedLeadingIconColor = neworange
+
+
+            )
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            modifier = Modifier.width(350.dp),
+            leadingIcon = {Icon(imageVector = Icons.Default.Lock, contentDescription = "")},
+            label = {Text(text = "Password")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = neworange,
+                focusedBorderColor = Color.DarkGray,
+                unfocusedLeadingIconColor = neworange
+
+
+            )
+        )
+
+        Spacer(modifier = Modifier.height(5.dp))
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(neworange),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.width(350.dp),
+        ) {
+            Text(text = "Login")
+
+
+        }
+        Text(
+            text = "Don't have an account? Register!",
+            fontSize = 20.sp,
+        )
+
+
+
+
+    }
+
+
+}
+@Preview(showBackground = true)
+@Composable
+fun ServiceScreenPreview(){
+    ServiceScreen()
+}
